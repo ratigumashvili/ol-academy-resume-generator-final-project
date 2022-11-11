6de58484f1ee4822fe2c12ca0b0abe57614558da
Rati <ratigumashvili@gmail.com>
2022-11-11 17:25:08 +0400

    finished template-box & templates

--- src/pages/Templates.jsx
+++ src/pages/Templates.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { createSearchParams, useNavigate } from "react-router-dom";
 import ColorPicker from "../components/ColorPicker";
 import TemplateBox from "../components/TemplateBox";
 
@@ -18,20 +18,61 @@ const pallete = [
   { id: 12, color: "#D39D2A", name: "Autumn Mustard" },
 ];
 
+const themes = [
+  {
+    id: 1,
+    name: "Angora",
+    desc: "When you know you're a natural fit, the conversation is warm, you get that fuzzy feeling, and every fiber of your being says you're perfect for this job, there's no better design than Angora.",
+  },
+  {
+    id: 2,
+    name: "Blueprint",
+    desc: "A place for everything and everything in it's place. Build your career on a solid foundation with the measured design elements in Blueprint. You're a total stud and you're sure to nail the interview.",
+  },
+];
+
 const Templates = () => {
   const [currentPallete, setCurrentPallete] = useState(0);
   const [currentColor, setCurrentColor] = useState("#000000");
+  const [currentTheme, setCurrentTheme] = useState(0);
+  const [theme, setTheme] = useState(themes[0]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTheme(themes[currentTheme]);
+  }, [currentTheme]);
 
   const handlePickColor = (item) => {
     setCurrentPallete(item.id - 1);
     setCurrentColor(item.color);
   };
+
+  const handleThemeChange = () => {
+    if (currentTheme === themes.length - 1) {
+      setCurrentTheme(0);
+    } else {
+      setCurrentTheme(currentTheme + 1);
+    }
+    setTheme(themes[currentTheme]);
+  };
+
+  const handleNavigateWithParams = () => {
+    navigate({
+      pathname: "/create",
+      search: createSearchParams({
+        theme: theme.name,
+        color: currentColor.toString().slice(2),
+      }).toString(),
+    });
+  };
+
   return (
     <div className="container">
-      <h2>Templates</h2>
       <div className="row">
-        <div className="col-sm-12 col-md-6">
-          Switch toggle <br />{" "}
+        <div className="col-sm-12 col-md-6 color-picker-holder">
+          <h2>{theme.name}</h2>
+          <p>{theme.desc}</p>
           <ColorPicker
             pallete={pallete}
             currentPallete={currentPallete}
@@ -40,10 +81,16 @@ const Templates = () => {
           />
         </div>
         <div className="col-sm-12 col-md-6">
-          <TemplateBox currentColor={currentColor} />
+          <TemplateBox
+            currentColor={currentColor}
+            theme={theme}
+            handleThemeChange={handleThemeChange}
+          />
         </div>
         <div className="col-sm-12">
-          <Link to="/create">Select This Theme</Link>
+          <button className="btn btn-cta" onClick={handleNavigateWithParams}>
+            Select this template
+          </button>
         </div>
       </div>
     </div>
